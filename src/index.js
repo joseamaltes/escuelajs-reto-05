d7df6fe5e042fb2348a5c810744105843b09a14e
Jose Antonio <josea_458@hotmail.com>
2019-10-02 18:08:26 -0500

    RetJS

--- src/index.js
+++ src/index.js
@@ -2,35 +2,40 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
+
+
+const loadData = async (par) => {
+  
+  let res = await fetch(par);
+  let response = await res.json();
+  const characters = response.results;
+   localStorage.setItem('next_fetch', JSON.stringify(response.info.next));
+        let output = characters.map(character => {
         return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
+    ` 
       }).join('');
       let newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
-
-const loadData = () => {
-  getData(API);
-}
+     $app.appendChild(newItem);};
 
 const intersectionObserver = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
-    loadData();
-  }
+loadData(API);
+if(JSON.parse(localStorage.getItem('next_fetch')) != ""){
+  loadData(JSON.parse(localStorage.getItem('next_fetch')))
+   console.log(JSON.parse(localStorage.getItem('next_fetch')));
+
+ } else {
+   localStorage.clear(); 
+   console.log( 'Ya no hay mas personajes');
+   intersectionObserver.disconnect();
+ }; 
+       }
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
